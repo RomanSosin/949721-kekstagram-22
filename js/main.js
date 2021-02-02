4fab79aa049051fa25b3fb89a4c947aa78dd4533
Roman <sosinrd@gmail.com>
2021-02-02 16:05:04 +0300

    Правки после проверки пулриквеста

--- js/main.js
+++ js/main.js
@@ -3,25 +3,27 @@
 
 function getRandomIntInclusive(min, max) {
   if (min >=0 && max >= 0 && max > min) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }//Максимум и минимум включаются
   else {
-  console.log("Введите диапазон от нуля до ...");
+    console.log('Введите диапазон от нуля до ...');
   }
 }
+
+console.log(getRandomIntInclusive(1, 10));
 
 
 // Функция для проверки максимальной длины строки.
 
-let commentString; // Нужно указывать где-то тип данных String?
-
 function getCommentStringsLength(commentString, max) {
 	if (commentString.length > max) {
     console.log('Строка больше ' + max + ' символов')
-    }
+  }
   else {
-  	console.log('Строка меньше ' + max + ' символов');
+    console.log('Строка меньше ' + max + ' символов');
   }
 }
+
+console.log(getCommentStringsLength('Функция для проверки максимальной длины строки', 10));

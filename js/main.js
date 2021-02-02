fc755057d99d3df666bef4b69762dd0752469a22
Roman <sosinrd@gmail.com>
2021-02-02 17:03:52 +0300

    Добавляем использование функции после проверки на пулреквест

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // Диапазон >= 0
 
 const getRandomIntInclusive = function(min, max) {
-  if (min >=0 && max >= co0 && max > min) {
+  if (min >=0 && max >= 0 && max > min) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -22,3 +22,5 @@ const getCommentStringsLength = function(commentString, max) {
     return false;
   }
 }
+
+getCommentStringsLength('Функция для проверки максимальной длины строки', 10);

44423b2ddee0ae5b786e96eec7d4ce221ac2a4e0
Roman <sosinrd@gmail.com>
2021-02-02 17:00:46 +0300

    убираем console.log из функций после проверки на пулреквесте

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 // Диапазон >= 0
 
-function getRandomIntInclusive(min, max) {
-  if (min >=0 && max >= 0 && max > min) {
+const getRandomIntInclusive = function(min, max) {
+  if (min >=0 && max >= co0 && max > min) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }//Максимум и минимум включаются
   else {
-    return console.log('Введите диапазон от нуля до ...');
+    return false;
   }
 }
 
 // Функция для проверки максимальной длины строки.
 
-function getCommentStringsLength(commentString, max) {
+const getCommentStringsLength = function(commentString, max) {
   if (commentString.length > max) {
-    return console.log('Строка больше ' + max + ' символов');
+    return true;
   }
   else {
-    return console.log('Строка меньше ' + max + ' символов');
+    return false;
   }
 }

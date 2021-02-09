0cd2723224e823b25d6b9bbef5d08fa922d1317f
Roman <sosinrd@gmail.com>
2021-02-09 08:18:47 +0300

    Формируем рандомный массив, первая попытка

--- js/main.js
+++ js/main.js
@@ -21,3 +21,76 @@ const isStringLengthValid = function(commentString, max) {
 }
 
 isStringLengthValid('Функция для проверки максимальной длины строки', 10);
+
+// Создаем рандомные массивы
+
+const DESCRIPTION  = [
+  'солнечный день',
+  'отличная прогулка',
+  'зимний лес',
+  'отдыхаем вместе',
+  'лучшая прогулка',
+  'вместе с друзьями',
+]
+
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const createRandomDescription = (elements) => {
+  return elements[getRandomIntInclusive(0, elements.length - 1)];
+};
+
+const getRandomDescription = () => {
+  return createRandomDescription(DESCRIPTION);
+}
+
+const getRandomComments = () => {
+  return createRandomDescription(COMMENTS);
+}
+
+const getRandomNames = () => {
+  return createRandomDescription(NAMES);
+}
+
+const getRandomComment = () => {
+  return {
+    id: 135,
+    avatar: 'img/avatar-' + getRandomIntInclusive(1, 6) + '.svg',
+    message: getRandomComments(),
+    name: getRandomNames(),
+  }
+}
+
+const CREATE_COMMENTS_COUNT = 25;
+
+const creatRandomComments = new Array(CREATE_COMMENTS_COUNT).fill(null).map(() => getRandomComment());
+
+
+const descriptionPhoto = {
+  id: getRandomIntInclusive(1, 25),
+  url: 'photos/' + getRandomIntInclusive(1, 25) + '.jpg',
+  description: getRandomDescription(),
+  likes: getRandomIntInclusive(15, 200),
+  comments: createRandomComments;
+}
+
+const CREATE_ARRAY_COUNT = 2;
+
+const createArray = new Array(CREATE_ARRAY_COUNT).fill(null).map(() => descriptionPhoto());

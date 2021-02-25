67b81cf4a8017a1eff13347535b0b8defcd02ae8
Roman <sosinrd@gmail.com>
2021-02-25 18:29:51 +0300

    отрисовываем миниатюры с помощью цикла и складываем в documentFragment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 // точка входа
-import './data.js';
+import './data.js'; // Генерируем массивы
+import './pictures.js'; // Создаем фото и добавляем в DocumentFragment
---
+++ js/pictures.js
@@ -0,0 +1,20 @@
+import {createCommentsArray, createPhotoArray} from 'data.js';
+
+const pictureFragment = document.createDocumentFragment(); // Складываем сюда
+
+const pictureTemplate = document.querySelector('#picture').content;
+const newPictureTemplate = pictureTemplate.querySelector('.picture');
+
+const createPicturesInHTML = () => {
+  for (let i = 0; i < photoArray.length, i++) {
+    const newPicture = newPictureTemplate.cloneNode(true);
+    newPicture.querySelector('.picture__img').src = photoArray[i].url;
+    newPicture.querySelector('.picture__likes').textContent = photoArray[i].likes;
+    newPicture.querySelector('.picture__comments').textContent = photoArray[i].comments;
+    pictureFragment.appendChild(newPicture);
+  }
+};
+
+createPicturesInHTML();
+
+export { createPicturesInHTML };
